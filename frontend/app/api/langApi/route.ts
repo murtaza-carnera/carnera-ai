dfd06a9db0da97bbea7067ed0c1e349c67746a17
Harsh Tenguriya <harsh.t@getcarnera.com>
2025-02-08 02:33:22 +0530

    FIxes and QA Add on

--- frontend/app/api/langApi/route.ts
+++ frontend/app/api/langApi/route.ts
@@ -1,14 +1,29 @@
 import { ai } from "@/services/lang";
 import { NextRequest } from "next/server";
 
-const graph = ai();
+const carneraAi = ai();
 
 export async function GET(request: NextRequest): Promise<Response> {
   try {
     const { searchParams } = new URL(request.url);
     let inputs = { question: searchParams.get('question')?.toString() };
-    const result = await (await graph).invoke(inputs);
-    return new Response(result.answer, { status: 200 });
+
+    const stream = await (await carneraAi).graph.stream(inputs, { streamMode: "messages" });
+
+    const readableStream = new ReadableStream({
+      async start(controller) {
+        for await (const [message, _metadata] of stream) {
+          controller.enqueue(new TextEncoder().encode(message.content));
+        }
+        controller.close();
+      }
+    });
+
+    return new Response(readableStream, {
+      headers: { "Content-Type": "text/plain" },
+      status: 200
+    });
+
   } catch (error) {
     console.error(error);
     return new Response('Error', { status: 500 });
--- frontend/src/services/lang.ts
+++ frontend/src/services/lang.ts
@@ -8,6 +8,16 @@ import { Annotation, StateGraph } from "@langchain/langgraph";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { vectorStore } from "@/db/db";
 import { llm } from "./carneraai";
+// import z from "zod";
+
+// const searchSchema = z.object({
+//   query: z.string().describe("Search query to run."),
+//   section: z.enum(["beginning", "middle", "end"]).describe("Section to query."),
+// });
+
+// const structuredLlm = llm.withStructuredOutput(searchSchema);
+
+
 
 export async function ai(){
     console.log("Hello from ai function");
@@ -30,10 +40,13 @@ const allSplits = await splitter.splitDocuments(docs);
 
 
 // Index chunks
+
+
+// const vectorStoreQA = new MemoryVectorStore(embeddings);
 await vectorStore.addDocuments(allSplits)
 
 // Define prompt for question-answering
-const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+const promptTemplate = await pull<ChatPromptTemplate>("rlm-carnera-ai-rag-1");
 
 // Define state for application
 const InputStateAnnotation = Annotation.Root({
@@ -46,11 +59,35 @@ const StateAnnotation = Annotation.Root({
   answer: Annotation<string>,
 });
 
+// const StateAnnotationQA = Annotation.Root({
+//   question: Annotation<string>,
+//   search: Annotation<z.infer<typeof searchSchema>>,
+//   context: Annotation<Document[]>,
+//   answer: Annotation<string>,
+// });
+
+// const analyzeQuery = async (state: typeof InputStateAnnotation.State) => {
+//   const result = await structuredLlm.invoke(state.question);
+//   return { search: result };
+// };
+
 // Define application steps
 const retrieve = async (state: typeof InputStateAnnotation.State) => {
   const retrievedDocs = await vectorStore.similaritySearch(state.question)
   return { context: retrievedDocs };
 };
+
+// const retrieveQA = async (state: typeof StateAnnotationQA.State) => {
+//   const filter = (doc: any) => doc.metadata.section === state.search.section;
+//   const retrievedDocs = await vectorStoreQA.similaritySearch(
+//     state.search.query,
+//     2,
+//     filter
+//   );
+//   return { context: retrievedDocs };
+// };
+
+
 
 
 const generate = async (state: typeof StateAnnotation.State) => {
@@ -59,6 +96,16 @@ const generate = async (state: typeof StateAnnotation.State) => {
   const response = await llm.invoke(messages);
   return { answer: response.content };
 };
+
+// const generateQA = async (state: typeof StateAnnotationQA.State) => {
+//   const docsContent = state.context.map((doc) => doc.pageContent).join("\n");
+//   const messages = await promptTemplate.invoke({
+//     question: state.question,
+//     context: docsContent,
+//   });
+//   const response = await llm.invoke(messages);
+//   return { answer: response.content };
+// };
 
 
 // Compile application and test
@@ -70,5 +117,17 @@ const graph = new StateGraph(StateAnnotation)
   .addEdge("generate", "__end__")
   .compile();
 
-return graph;
+  // const graphQA = new StateGraph(StateAnnotationQA)
+  // .addNode("analyzeQuery", analyzeQuery)
+  // .addNode("retrieveQA", retrieveQA)
+  // .addNode("generateQA", generateQA)
+  // .addEdge("__start__", "analyzeQuery")
+  // .addEdge("analyzeQuery", "retrieveQA")
+  // .addEdge("retrieveQA", "generateQA")
+  // .addEdge("generateQA", "__end__")
+  // .compile();
+
+return {graph};
+
+
 }
